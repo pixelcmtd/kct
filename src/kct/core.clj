7149c5053d96826c3909dcb9351c4c5dc40b4197
chrissx <chrissx@chrissx.de>
2021-02-06 15:58:54 +0100

    wrote like a poc

--- src/kct/core.clj
+++ src/kct/core.clj
@@ -1,7 +1,32 @@
 (ns kct.core
   (:gen-class))
 
+(defn print-slide [keynote i]
+  (print "\033[1;1H\033[J")
+  (println "" (get keynote :title) (str "(" (inc i) "/" (count (get keynote :slides)) ")"))
+  (println)
+  (doall (map println (nth (get keynote :slides) i)))
+  (println)
+  (println "" (get keynote :ad)))
+
+(defn kct-loop [keynote i]
+  (print-slide keynote i)
+  (kct-loop keynote
+    (let [l (read-line)]
+      (cond
+        (= l "n") (inc i)
+        (= l "p") (dec i)
+        (= l "h") (dec i)
+        (= l "j") (inc i)
+        (= l "k") (dec i)
+        (= l "l") (inc i)
+        (= l "q") (System/exit 0)
+        :else i))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Parses args and starts KCT."
   [& args]
-  (println " hello, world!"))
+  (kct-loop
+    { :title "my wonderful presentation"
+      :slides '[["line 1" "line 2" "line 3"]["lol"]["lel"]]
+      :ad "Read ZERM! https://zerm.eu" } 0))
