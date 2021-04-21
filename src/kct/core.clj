3a674cea4889686779633ca6282ab02cb69a2728
chrissx <chrissx@chrissx.de>
2021-04-22 00:42:50 +0200

    Added a shortcut for jumping to a specific page

--- src/kct/core.clj
+++ src/kct/core.clj
@@ -10,16 +10,17 @@
   (if (= i (count (keynote :slides))) (System/exit 0) nil)
   (doall (map println (str-slide keynote i)))
   (kct-loop keynote
-    (let [l (read-line)]
+    (let [l (read-line) p (try (Integer/parseInt l) (catch Exception e nil))]
       (cond
-        (= l "")  (inc i)
-        (= l "n") (inc i)
-        (= l "p") (dec i)
-        (= l "h") (dec i)
-        (= l "j") (inc i)
-        (= l "k") (dec i)
-        (= l "l") (inc i)
-        (= l "q") (System/exit 0)
+        (not (nil? p)) (dec p)
+        (= l "")       (inc i)
+        (= l "n")      (inc i)
+        (= l "p")      (dec i)
+        (= l "h")      (dec i)
+        (= l "j")      (inc i)
+        (= l "k")      (dec i)
+        (= l "l")      (inc i)
+        (= l "q")      (System/exit 0)
         :else i))))
 
 (defn -main [& args]
